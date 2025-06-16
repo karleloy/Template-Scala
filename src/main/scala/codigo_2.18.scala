3b7b2c172fa088f9e087cfe15db8ddd2ce376aae
karleloy <135286259+karleloy@users.noreply.github.com>
2025-06-16 17:50:54 +0000

    coreções

--- src/main/scala/codigo_2.18.scala
+++ src/main/scala/codigo_2.18.scala
@@ -2,6 +2,6 @@ def buildList(n: Int): List[Int] =
     if (n <= 0) List() // Caso base: lista vazia
     else n :: buildList(n - 1) // Caso recursivo: adiciona n no início da lista 
 
-@ main def main(): Unit = 
+@main def main(): Unit = 
     val list = buildList(10)
     println(s"List from 1 to 10: $list")
--- src/main/scala/codigo_2.4.scala
+++ src/main/scala/codigo_2.4.scala
@@ -3,6 +3,6 @@ def  sayName = "Karl Eloy"
 def getAge(yearOfBirth: Int) = 
     java.time.Year.now.getValue - yearOfBirth
 
-@main def main() = 
+@main def expEstruturaFuncional() = 
     println("Name: " + sayName)
     println("Age: " + getAge(1986))
