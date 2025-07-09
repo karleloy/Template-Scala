41f2d450cc177791f5cb351e66d842b2fd4d7947
karleloy <135286259+karleloy@users.noreply.github.com>
2025-07-09 19:39:24 +0000

    aula 04

---
+++ src/main/scala/04-recursividade/codigo_4.6-4.7.scala
@@ -0,0 +1,17 @@
+//recursiva na cauda para o problema de somar os números de a
+//até b.
+
+package tailrecursivesum
+
+//@tailrec
+def tailRecursiveSum(a: Long, b: Long, acc: Long = 0): Long = {
+    if (a > b) acc
+    else tailRecursiveSum(a + 1, b, acc + a) //+ 0
+}
+
+@main def run(): Unit = {
+    println(tailRecursiveSum(1, 10)) 
+    println(tailRecursiveSum(5, 15)) 
+    println(tailRecursiveSum(0, 100))
+    println(tailRecursiveSum(1, 1_000_000))
+}
